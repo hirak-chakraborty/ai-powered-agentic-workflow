bc3d36162144ffc1f9d6bc9384527e415fcde015
Hirak Jyoti Chakraborty <hirakchakraborty99@gmail.com>
2026-07-12 20:13:25 +0530

    Worked on comment #1

--- starter/phase_2/workflow_agents/base_agents.py
+++ starter/phase_2/workflow_agents/base_agents.py
@@ -296,7 +296,7 @@ class EvaluationAgent:
 
             print(" Step 3: Check if evaluation is positive")
             if evaluation.lower().startswith("yes"):
-                print("✅ Final solution accepted.")
+                print("Final solution accepted.")
                 break
             else:
                 print(" Step 4: Generate instructions to correct the response")
